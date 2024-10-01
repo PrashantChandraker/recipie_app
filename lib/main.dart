398ec809082542ccaf96fc468f79ff121179c142
PrashantChandraker <prashantchandraker1@gmail.com>
2024-10-01 09:47:25 +0530

    Firebase connected and packages imported

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [NavigationBar].
+
+void main() => runApp(const NavigationBarApp());
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+     
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("data"),
+      ),
+      body: const Column(
+        children: [
+          Text("dadssdfta"),
+        ],
+      ),
+    );
+  }
+}
